79314ac7a48239a741eb8568b853e54877acda89
Keir Staple <keirstaple@comcast.net>
2016-03-25 13:19:29 -0700

    add food component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { FoodListComponent } from './food-list.component';
 import { Food } from './food.model';
 
@@ -10,10 +10,13 @@ import { Food } from './food.model';
       <h1>Food Log</h1>
       <food-list
         [foodList]="foods">
+        (onFoodSelect)="foodWasSelected($event)"
       </food-list>
     </div>
   `
-  //[foodList] is an input instantiated in food-list from the FoodListComponent. We are setting it equal to the property foods, where Food[] identifies foods as an array of Food objects. 
+  //[foodList] is an input instantiated in food-list from the FoodListComponent. We are setting it equal to the property foods, where Food[] identifies foods as an array of Food objects.
+
+  //tell the <food-list> component to connect its output onFoodSelect, our custom event emitter, to the parent method foodWasSelected. Because it is a custom event, we receive the actual event itself, defined by the argument $event. Then, inside of the method we can treat it as a normal Food object, because this is what we passed in from the child component when we emitted the event.
 })
 
 export class AppComponent {
@@ -24,6 +27,6 @@ export class AppComponent {
       ];
   }
   foodWasSelected(clickedFood: Food): void {
-    console.log(clickedFood);
+    console.log('parent', clickedFood);
   }
 }
--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,19 +1,34 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { FoodComponent } from './food.component';
 import { Food } from './food.model';
 
 @Component({
   selector: 'food-list',
-  inputs: 'foodList',
+  inputs: ['foodList'],
+  outputs: ['onFoodSelect'],
+  directives: [FoodComponent],
   template: `
-    <h3 *ngFor="#currentFood of foodList" (click)="foodWasClicked(currentFood)">
-      {{ currentFood.name }}
-    </h3>
+    <food-display *ngFor="#currentFood of foodList"
+      (click)="foodWasClicked(currentFood)"
+      [class.selected]="currentFood === selectedFood"
+      [food]="currentFood">
+    </food-display>
   `
+  //[class.selected]="currentTask === selectedTask" tells Angular to either add or remove the class selected based on whether or not the condition to the right of the equals sign is true: currentTask === selectedTask
+
+  //[food]="currentFood" sets the task input equal to the local variable currentTask as we instantiate the <task-display> tags with our for each loop
 })
 
 export class FoodListComponent {
-  public foodList: Food[];
+  public foodList: Food[]; //setting foodList propety to expect a Food array. We will send this to the app component, where we will set it equal to the foods property, which is also an array of Food objects.
+  public onFoodSelect: EventEmitter<Food>; //create a property to hold the Event Emitter object FoodListComponent output
+  public selectedFood: Food;
+  constructor(){
+    this.onFoodSelect = new EventEmitter();
+  }
   foodWasClicked(clickedFood: Food): void {
-    console.log(clickedFood);
+    console.log('child', clickedFood);
+    this.selectedFood = clickedFood;
+    this.onFoodSelect.emit(clickedFood);
   }
 }
