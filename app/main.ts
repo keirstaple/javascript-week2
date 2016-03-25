d4b9548882dde7616a1e94c19cacf0335334520f
Keir Staple <keirstaple@comcast.net>
2016-03-25 08:31:29 -0700

    set up initial package manager and its dependencies, as well as initial files

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap } from 'angular2/platform/browser'
+import { AppComponent } from './app.component'
+
+bootstrap(AppComponent);
+
+//when we bootstrap our root component we are loading our entire app and indicating to Angular that this is the entry point to our application that will load the rest of it for us//
