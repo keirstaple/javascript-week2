e400a5a85264bccc1b8beb41fe228e96857c9291
Keir Staple <keirstaple@comcast.net>
2016-03-30 13:15:44 -0700

    add some comments to explain the code

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -14,8 +14,6 @@ import { HealthyPipe } from './healthy.pipe';
   directives: [FoodComponent, EditFoodComponent, NewFoodComponent, DisplayDetailsCaloriesComponent],
   pipes: [HealthyPipe],
   template: `
-
-
     <select (change)="onChange($event.target.value)">
       <option value="all" selected="selected">Show All</option>
       <option value="healthy">Show Healthy</option>
@@ -35,11 +33,12 @@ import { HealthyPipe } from './healthy.pipe';
       <new-food (onSubmitNewFood)="createFoodEntry($event)"></new-food>
     </div>
   `
-  //[class.selected]="currentTask === selectedTask" tells Angular to either add or remove the class selected based on whether or not the condition to the right of the equals sign is true: currentTask === selectedTask
 
-  //[food]="currentFood" sets the task input equal to the local variable currentTask as we instantiate the <task-display> tags with our for each loop
+  //Connect the (change) event using parenthesis for output to a method called onChange. Pass it the selected value from the menu by using the argument $event.target.value.
 
-  //When a food is clicked, we use the (click) emitter to call our foodClicked method. This sets the FoodListComponent's property selectedFood equal to the task that we want to edit. Therefore, we can just pass this property value into the EditFoodComponent.
+  //[food]="currentFood" sets the food input equal to the local variable currentFood as we instantiate the <food-display> tags with our for each loop
+
+  //When a food is clicked, we use the (click) emitter to call our foodClicked method. This sets the FoodListComponent's property selectedFood equal to the food that we want to edit. Therefore, we can just pass this property value into the EditFoodComponent.
 
   //Use *ngIf as an attribute on an HTML tag. Set it to an expression in double quotes, and if the expression evaluates to true (or at least to not false), then the HTML tag (and any other nested tags) are shown. If the expression evaluates to false or undefined, then the HTML tag is hidden.
 })
--- app/healthy.pipe.ts
+++ app/healthy.pipe.ts
@@ -1,13 +1,16 @@
+
 import { Pipe, PipeTransform } from 'angular2/core';
 import { Food } from './food.model';
 
 @Pipe({
   name: "healthy",
   pure: false
+  //Declaring a pipe to be stateful (false) tells Angular to check and see if the output of the pipe has changed after each change detection cycle, this causes it to update as soon as we have changed something about a task.
 })
 
 export class HealthyPipe implements PipeTransform {
   transform(input: Food[], args) {
+  //args is the traditional name for a function parameter that includes multiple pieces of information like this. Generally, in our variable names we want to avoid abbreviations, but this one is so widely used that it has become kind of a standard.
     var isHealthy = args[0];
     if (isHealthy === "healthy") {
       return input.filter((food) => {
